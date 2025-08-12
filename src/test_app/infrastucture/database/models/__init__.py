225ee7f02e4d06ced2bb141ede80d8681da6f229
AJlekCandr-proff` <svznikov@yandex.ru>
2025-08-12 11:30:02 +0300

    feat: Imports alchemy-models.

---
+++ src/test_app/infrastucture/database/models/__init__.py
@@ -0,0 +1,6 @@
+from test_app.infrastucture.database.models.base import Base
+from test_app.infrastucture.database.models.transaction import Transaction
+from test_app.infrastucture.database.models.user import User
+
+
+__all__ = ["Base", "Transaction", "User"]
