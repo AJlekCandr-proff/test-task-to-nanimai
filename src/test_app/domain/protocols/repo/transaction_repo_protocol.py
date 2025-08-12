8745672779e2972b3faea777ef06f99ee12c1350
AJlekCandr-proff` <svznikov@yandex.ru>
2025-08-12 12:06:28 +0300

    feat: Create transaction_repo_protocol.py.

---
+++ src/test_app/domain/protocols/repo/transaction_repo_protocol.py
@@ -0,0 +1,23 @@
+from decimal import Decimal
+from typing import Protocol
+from uuid import UUID
+
+from test_app.domain.transaction.model import TransactionModel
+
+
+class TransactionReadRepositoryProtocol(Protocol):
+    async def get_transaction(self, transaction_uuid: UUID) -> TransactionModel:
+        raise NotImplementedError()
+
+
+class TransactionWriteRepositoryProtocol(Protocol):
+    async def open_transaction(
+        self, token: str, save_sum: Decimal, user_uuid: UUID
+    ) -> TransactionModel:
+        raise NotImplementedError()
+
+    async def close_transaction(self, transaction_uuid: UUID) -> TransactionModel:
+        raise NotImplementedError()
+
+    async def cancel_transaction(self, transaction_uuid: UUID) -> TransactionModel:
+        raise NotImplementedError()
