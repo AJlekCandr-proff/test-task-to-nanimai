be0460018a93a9bf7f2d2982107191e6fed282f4
AJlekCandr-proff` <svznikov@yandex.ru>
2025-08-12 10:30:09 +0300

    feat: Create application.py. Initialization app.

---
+++ src/test_app/application/application.py
@@ -0,0 +1,31 @@
+from dishka import make_async_container
+from dishka.integrations.fastapi import FastapiProvider, setup_dishka
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import ORJSONResponse
+
+
+def setup_containers(app: FastAPI) -> None:
+    container = make_async_container()
+
+    setup_dishka(container, app)
+
+
+def create_app() -> FastAPI:
+    app = FastAPI(default_response_class=ORJSONResponse)
+
+    setup_containers(app)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:5173", "http://127.0.0.0:5173"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+        expose_headers=["*"],
+    )
+
+    return app
+
+
+app = create_app()
