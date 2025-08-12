fdb88ae6af67d32f55e5d79cb80ab6521fca3573
AJlekCandr-proff` <svznikov@yandex.ru>
2025-08-12 11:13:09 +0300

    feat: Update user.py. Addition default property for fields.

--- src/test_app/infrastucture/database/models/user.py
+++ src/test_app/infrastucture/database/models/user.py
@@ -8,7 +8,10 @@ from test_app.infrastucture.database.models.base import Base
 class User(Base):
     name: Mapped[str] = mapped_column(String, comment="Имя пользователя")
 
-    balance: Mapped[str] = mapped_column(String, comment="Баланс пользователя")
+    balance: Mapped[str] = mapped_column(
+        String, default="0", comment="Баланс пользователя"
+    )
+
     max_balance: Mapped[str] = mapped_column(
-        String, comment="Максимально возможный баланс пользователя"
+        String, default="1000", comment="Максимально возможный баланс пользователя"
     )
