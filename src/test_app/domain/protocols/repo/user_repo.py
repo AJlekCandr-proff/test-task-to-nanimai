e17671b4598967f4abcf2c64f85a0e8439c21b6c
AJlekCandr-proff` <svznikov@yandex.ru>
2025-08-12 11:58:17 +0300

    feat: Create user-repositories protocols.

---
+++ src/test_app/domain/protocols/repo/user_repo.py
@@ -0,0 +1,22 @@
+from decimal import Decimal
+from typing import Protocol
+from uuid import UUID
+
+from test_app.domain.user.model import UserModel
+
+
+class UserReadRepositoryProtocol(Protocol):
+    async def get_user(self, user_uuid: UUID) -> UserModel:
+        raise NotImplementedError()
+
+
+class UserWriteRepositoryProtocol(Protocol):
+    async def set_current_balance_user(
+        self, user_uuid: UUID, max_balance: Decimal
+    ) -> UserModel:
+        raise NotImplementedError()
+
+    async def set_max_balance_user(
+        self, user_uuid: UUID, max_balance: Decimal
+    ) -> UserModel:
+        raise NotImplementedError()
