56c23e32e5e639ba68731fc7bcd568d091a5a74f
AJlekCandr-proff` <svznikov@yandex.ru>
2025-08-12 11:43:03 +0300

    feat & fix: Update fields of alchemy-user model.

--- src/test_app/infrastucture/database/models/user.py
+++ src/test_app/infrastucture/database/models/user.py
@@ -1,6 +1,7 @@
+from decimal import Decimal
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String
+from sqlalchemy import String, DECIMAL
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from test_app.infrastucture.database.models import Base
@@ -12,12 +13,16 @@ if TYPE_CHECKING:
 class User(Base):
     name: Mapped[str] = mapped_column(String, comment="Имя пользователя")
 
-    balance: Mapped[str] = mapped_column(
-        String, default="0", comment="Баланс пользователя"
+    balance: Mapped[Decimal] = mapped_column(
+        DECIMAL(precision=10, scale=2),
+        default=Decimal("0.00"),
+        comment="Баланс пользователя",
     )
 
-    max_balance: Mapped[str] = mapped_column(
-        String, default="1000", comment="Максимально возможный баланс пользователя"
+    max_balance: Mapped[Decimal] = mapped_column(
+        DECIMAL(precision=10, scale=2),
+        default=Decimal("1000.00"),
+        comment="Максимально возможный баланс пользователя",
     )
 
     transactions: Mapped["Transaction"] = relationship(
